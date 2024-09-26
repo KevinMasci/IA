344401e2d10bbeb7683f91660f535ef7f0c5714b
AnaPaulaBrajus <102190220+AnaPaulaBrajus@users.noreply.github.com>
2024-09-26 23:45:02 +0000

    Ejercicio 9 y 12 practica 1

---
+++ P1/E9.pl
@@ -0,0 +1,19 @@
+/*RECURSIVIDAD
+9. Se tiene la siguiente base de hechos:
+hijo(juan,miguel).
+hijo(jose,miguel).
+hijo(miguel,roberto).
+hijo(julio,roberto).
+hijo(roberto,carlos).
+Donde hijo(X,Y) indica que X es hijo de Y.
+Definir la regla descendiente(A,B), la cual permite determinar si A es
+descendiente de B.*/
+
+hijo(juan,miguel).
+hijo(jose,miguel).
+hijo(miguel,roberto).
+hijo(julio,roberto).
+hijo(roberto,carlos).
+
+descendiente(X,Y):-hijo(X,Y).
+descendiente(X,Y):-hijo(X,Z), descendiente(Z,Y).
