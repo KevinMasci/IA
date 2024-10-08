c1e97490146828bf945bf9be4f9f1bbfb694d680
AnaPaulaBrajus <102190220+AnaPaulaBrajus@users.noreply.github.com>
2024-10-08 23:24:11 +0000

    Se elimina final repetido

--- Finales/241018 Rec AD.pl
+++
@@ -1,7 +0,0 @@
-/*usuario(id, estado, edad, ciudad, [ids usuarios amigos]).
-provincia(id, nombre, [ids ciudades]).
-pais(id, nombre, [ids provincias]).
-El estado de un usuario puede ser activo o inactivo.
-1) Mostrar la cantidad de usuarios activos que vivan en países que tengan más de 20 provincias.
-2) Ingresar los datos de un usuario (id, estado, edad, ciudad, [ids usuarios amigos]) y guardarlo en la base de datos sólo si es argentino y mayor de 18 años.
-*/
