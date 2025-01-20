2e95637c9305ae5103c3c91014e176eaad7d975d
Kevin Masci <86538021+KevinMasci@users.noreply.github.com>
2025-01-20 20:35:58 -0300

    Update E13.pl

--- P2/E13.pl
+++ P2/E13.pl
@@ -1,6 +1,4 @@
-% Ingresar dos listas de elementos, concatenarlas (los elementos deben
-ser asignados de a uno en la lista de salida) y mostrarlas en una
-tercera.
+%Ingresar dos listas de elementos, concatenarlas (los elementos deben ser asignados de a uno en la lista de salida) y mostrarlas en una tercera.
 
 inicio:-write('ingrese lista 1: '), leer(L1), write('ingrese lista 2: '), leer(L2),
          concatenar(L1,L2,L3), writeln(L3).
