f4b0daebc6294ed42bbcb87d6042cd9f266737d7
Kevin Masci <86538021+KevinMasci@users.noreply.github.com>
2025-01-20 19:56:04 -0300

    Update E10.pl

--- P2/E10.pl
+++ P2/E10.pl
@@ -1,4 +1,15 @@
 /*10. Ingresar una lista y un elemento e informar si ese elemento está en la
 lista.*/
 
+inicio:-write('ingrese lista: '), leer(L), writeln('ingrese un elemento a buscar: ')
+        , read(E), buscar_elemento(L,E).
+
+leer([H|T]):-read(H), H\=[], leer(T).
+leer([]).
+
+buscar_elemento([],_):-writeln('el elemento no se encuentra en la lista').
+buscar_elemento([H|_],H):-writeln('el elemento se encuentra en la lista').
+buscar_elemento([_|T],E):- buscar_elemento(T,E).
+
+
 %VER DIAPOSITIVA
